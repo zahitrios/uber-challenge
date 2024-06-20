9c17e5631a906aa9e2d175b2dd283e512c8251d7
zahitrios <zahit@xenotech.mx>
2024-06-20 14:47:55 -0600

    Fanciest car solutions done

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 To run exercises:
 
 Fares challenge:
+
 `tsc fares.tsx && node fares.js`
+
+Get fanciest allowed car:
+
+`tsc fanciestCar.tsx && node fanciestCar.js`
--- fanciestCar.tsx
+++ fanciestCar.tsx
@@ -1,16 +1,57 @@
+const carTypes = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
+
 type getCarRequest = {
 	l: number;
 	fares: number[];
 };
 
-function getCar({ l, fares }: getCarRequest): string {}
+const budget = 10;
 
-const a = performance.now();
-console.log(
-	getCar({
-		l: 30,
-		fares: [0.3, 0.5, 0.7, 1, 1.3],
-	})
-);
-const b = performance.now();
-console.log(`Execution time: ${b - a} ms.`);
+function slowSolution({ l, fares }: getCarRequest): string | null {
+	const allowedCarTypes = carTypes.reduce((acc: string[], carType: string, index: number) => {
+		if (budget >= l * fares[index]) acc.push(carType);
+		return acc;
+	}, []);
+
+	return allowedCarTypes.pop() ?? null;
+}
+
+function fastSolution({ l, fares }: getCarRequest): string | null {
+	let allowedCarType: string | null = null;
+
+	carTypes.forEach((carType: string, index: number) => {
+		if (budget >= l * fares[index]) allowedCarType = carType;
+		else return;
+	}, []);
+	return allowedCarType;
+}
+
+function fasterSolution({ l, fares }: getCarRequest): string | null {
+	let allowedCarType: string | null = null;
+	for (let index = 0; budget >= l * fares[index]; index++) allowedCarType = carTypes[index];
+	return allowedCarType;
+}
+
+let a = performance.now();
+let fanciestCar = slowSolution({
+	l: 30,
+	fares: [0.3, 0.5, 0.7, 1, 1.3],
+});
+let b = performance.now();
+console.log(`${fanciestCar} -> Slow solution execution time: ${b - a} ms.`);
+
+a = performance.now();
+fanciestCar = fastSolution({
+	l: 30,
+	fares: [0.3, 0.5, 0.7, 1, 1.3],
+});
+b = performance.now();
+console.log(`${fanciestCar} -> Faster solution execution time: ${b - a} ms.`);
+
+a = performance.now();
+fanciestCar = fasterSolution({
+	l: 30,
+	fares: [0.3, 0.5, 0.7, 1, 1.3],
+});
+b = performance.now();
+console.log(`${fanciestCar} -> Fastest solution execution time: ${b - a} ms.`);
